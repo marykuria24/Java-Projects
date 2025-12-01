2bd27abd0905d76711216a60f4c14b50c3106ae6
marykuria24 <wanjiruu27@gmail.com>
2025-12-01 13:11:31 +0300

    Update Main-1.java

--- Main-1.java
+++ Main-1.java
@@ -9,12 +9,13 @@ class Main {
     public static void main(String[] args) {
         Car myCar = new Car();
         myCar.brand = "Toyota";
-        myCar.year = 2024;
-        myCar.seats = 10;
+        myCar.year = 2017;
+        myCar.seats = 4;
 
         System.out.println("Car Details:");
         System.out.println("Brand: " + myCar.brand);
         System.out.println("Year: " + myCar.year);
         System.out.println("Seats: " + myCar.seats);
     }
+
 }
